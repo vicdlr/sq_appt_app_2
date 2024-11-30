1ada38e0a5542661f20afddf61f15df3e37225ff
ashish mittal <coderoofitsolutions@gmail.com>
2024-11-30 01:43:20 -0500

    add user id in url

--- lib/view/home/get_ticket.dart
+++ lib/view/home/get_ticket.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+
+import '../../SharedPrefrence/SharedPrefrence.dart';
 
 class GetTicket extends StatefulWidget {
   const GetTicket({super.key});
@@ -48,14 +52,14 @@ class _GetTicketState extends State<GetTicket> {
           ],
         ),
         Expanded(
-          child: isLoading 
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : MobileScanner(
-                controller: controller,
-                onDetect: _onDetect,
-              ),
+          child: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : MobileScanner(
+                  controller: controller,
+                  onDetect: _onDetect,
+                ),
         ),
       ],
     );
@@ -63,10 +67,10 @@ class _GetTicketState extends State<GetTicket> {
 
   void _onDetect(BarcodeCapture capture) async {
     final List<Barcode> barcodes = capture.barcodes;
-    
+
     for (final barcode in barcodes) {
       if (barcode.rawValue != null) {
-        final String url = barcode.rawValue!;
+        String url = barcode.rawValue!;
         setState(() {
           isLoading = true;
         });
@@ -76,11 +80,11 @@ class _GetTicketState extends State<GetTicket> {
 
         // Navigate to WebView
         if (mounted) {
-          
           await Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => WebViewPage(url: url),
+              builder: (context) =>
+                  WebViewPage(url: url + "/${SharedPref.getUserData().id}"),
             ),
           );
 
@@ -113,6 +117,7 @@ class WebViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log("url $url");
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ticket Details'),
